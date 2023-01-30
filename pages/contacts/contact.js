014d3d98406b4f7e0ede9be1bdeb68e0a4161624
Gleb Kostrov <anegada1@mail.ru>
2023-01-30 12:00:47 +0300

    С помощью утилиты npx create-next-app развернул web приложение на фрэймворке next js

---
+++ pages/contacts/contact.js
@@ -0,0 +1,6 @@
+const Contact = () => (
+    <h1>Jack</h1>
+  );
+  
+  export default Contact;
+  
---
+++ pages/contacts/index.js
@@ -0,0 +1,9 @@
+const Contacts = () => (
+    <>
+      <h1>Contacts list:</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adip</p>
+    </>
+  );
+  
+  export default Contacts;
+  
